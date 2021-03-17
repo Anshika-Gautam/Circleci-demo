169f260b2b09a42b611e98eb6a93a3f7b8adbb88
Anshika-Gautam <Anshika.Gautam@ibm.com>
2021-03-17 14:11:12 +0530

    latest monitor-ingest

--- monitor_ingest/README.md
+++ monitor_ingest/README.md
@@ -2,7 +2,7 @@
 
 This pack uses Maximo Monitor SDK (https://github.com/ibm-watson-iot/maximo-asset-monitor-sdk.git) in StackStorm dynamically. It has following features:
 
-- Create Entities in Maximo Assset Monitor
+- Create Entities in Maximo Asset Monitor
 - Add Constants, Dimensions and Functions
 - Cleaning the CSV data files as per a valid schema
 - Load metrics data from a CSV to Maximo
@@ -17,13 +17,15 @@ Python 3.7
 
 ## Setup
 
-### Install Monitor Ingest pack on local StackStorm env
-1. Clone from Github repo [Monitor Ingest_pack](https://github.ibm.com/Watson-IoT/monitor_ingest):
+### Install IBM Maximo Monitor Ingest pack on local StackStorm env
+1. Clone from Github repo [IBM Maximo Monitor Ingest_pack](https://github.com/maximo-developer/monitor_ingest):
 
     ```
     # clone Monitor_Ingest code from github repo
-    cd /opt/stackstorm/packs/
-    git clone --branch st2 https://github.ibm.com/Watson-IoT/monitor_ingest.git
+    git clone --branch st2 https://github.com/maximo-developer/monitor_ingest.git
+   
+    # Once pack will be published One can Install from Exchange by pack name:
+    st2 pack install monitor_ingest
     ```
 ## Get credentials
 2. Set credentials to connect Watson Analytics Service:
@@ -33,7 +35,7 @@ Python 3.7
 
 ## Configuration
 
-3. Copy the example configuration in [monitor_ingest.yaml.example](./monitor_ingest.yaml.example)
+3. Copy the example configuration in [monitor_ingest.yaml.example](monitor_ingest.yaml.example)
 to `/opt/stackstorm/configs/monitor_ingest.yaml` and edit as per credentials copied in Step 2.
 
 Config file must have action_type, data_file_path and credentials keys.
@@ -42,12 +44,12 @@ To run a specific Actions use `action_type`, `data_file_path` and `credentials` 
 `data_file_path` : source json input file for specified action type
 `credentials` : Analytics service credentials
  
-** Check [config.schema.yaml](./config.schema.yaml) schema before creating your config file.
+** Check [config.schema.yaml](config.schema.yaml) schema before creating your config file.
 
 Example configuration:
 
 ```yaml
-action_type : "SetupEntityAction | SetupAddConstants | SetupAddDimesions | SetupAddFunctions "
+action_type : "SetupEntityAction | SetupAddConstants | SetupAddDimensions | SetupAddFunctions "
 entity_name : ""
 data_file_path : "/opt/stackstorm/packs/monitor_ingest/etc/sample_usage_data.json"
 credentials:
@@ -90,13 +92,8 @@ credentials:
       mh_topic_analytics_alerts: ''
 ```
 
-**Note** : When modifying the configuration in `/opt/stackstorm/configs/` please
-           remember to tell StackStorm to load these new values by running
-           `sudo st2ctl reload --register-configs`
+## Creating Vitual environment in StackStorm (Required only if testing in St2 env):
 
-## Creating Vitual environment in StackStorm (Required only if testing in St2 env)
-
-    ```
     # 1. Setup monitor_ingest Virtual Env in SackStorm 
     st2 run packs.setup_virtualenv packs=monitor_ingest python3=True
     
@@ -108,7 +105,6 @@ credentials:
     
     # 4. Run an action
     st2 run <pack.action_name>
-    ```
 
 ## Actions
 
@@ -118,22 +114,22 @@ The following actions are supported:
 `[Mandatory]`
 * To Setup Entity Action use action_type - ``SetupEntityAction``
 * To Add Constants to an Entity use action_type - ``SetupAddConstants``
-* To Add Dimesions to an Entity use action_type - ``SetupAddDimesions``
+* To Add Dimensions to an Entity use action_type - ``SetupAddDimensions``
 * To Add Functions to an Entity use action_type - ``SetupAddFunctions``
 * Specify json file path in config -``data_file_path`` 
 #### Check /etc directory for Sample data files for Setup Entity Action
-* for SetupEntityAction sample [data_file_path](./etc/sample_usage_data.json)
-* for SetupAddConstants sample [data_file_path](./etc/sample_constant_data.json)
-* for SetupAddDimesions sample [data_file_path](./etc/sample_dimension_data.json)
-* for SetupAddFunctions sample [data_file_path](./etc/sample_function_data.json)
+* for SetupEntityAction sample [data_file_path](etc/sample_usage_data.json)
+* for SetupAddConstants sample [data_file_path](etc/sample_constant_data.json)
+* for SetupAddDimensions sample [data_file_path](etc/sample_dimension_data.json)
+* for SetupAddFunctions sample [data_file_path](etc/sample_function_data.json)
 
 ### Clean CSV metrics data from a CSV ``clean_csv_data``
 `[Mandatory]`
 * Specify json Schema file path in config -``json_schema_path``
 * Specify csv file path in config -``data_file_path``
 #### Check /etc directory for Sample data files for clean_csv_data Action
-* for clean_csv_data sample [data_file_path](./etc/sample_csv_data.csv)
-* for clean_csv_data sample [json_schema_path](./etc/csvSchema.json)
+* for clean_csv_data sample [data_file_path](etc/sample_csv_data.csv)
+* for clean_csv_data sample [json_schema_path](etc/csvSchema.json)
 ``Action Output`` - clean and error csv files will be under ./etc/clean_data_output/__.csv
 
 ### Data metrics data ingestion using CSV ``csv_data_ingest``
@@ -141,7 +137,7 @@ The following actions are supported:
 * Specify json file path in config -``data_file_path``
 * Specify entity name in config - ``entity_name``
 #### Check /etc directory for Sample data files for csv_data_ingest Action
-* for csv_data_ingest [sample data file](./etc/sample_csv_data.csv)
+* for csv_data_ingest [sample data file](etc/sample_csv_data.csv)
 * or for a clean csv please check under ./etc/clean_data_output/
 
 
--- monitor_ingest/actions/setup_entity.py
+++ monitor_ingest/actions/setup_entity.py
@@ -12,7 +12,7 @@ class SetupEntityAction(Action):
         self._credentials = self._config.get('credentials', None)
         self._data_file_path = self._config.get('data_file_path', None)
         self._action_type = self._config.get('action_type', None)
-        
+
         if not self._config:
             raise ValueError('Missing config yaml')
         if not self._credentials:
@@ -23,76 +23,65 @@ class SetupEntityAction(Action):
             raise ValueError('Missing action type key in config file')
 
     def run(self):
-        operations_completed = {}
-        entitytype_data_path = None
-        constants_data_path = None
-        dimension_data_path = None
-        function_data_path = None
-        
+        status = False
         if self._action_type == "SetupEntityAction":
-            entitytype_data_path = self._data_file_path
+            self.setup_EntityAction()
         elif self._action_type == "SetupAddConstants":
-            constants_data_path = self._data_file_path
-        elif self._action_type == "SetupAddDimesions":
-            dimension_data_path = self._data_file_path       
+            self.setup_AddConstants()
+        elif self._action_type == "SetupAddDimensions":
+            self.setup_AddDimensions()
         elif self._action_type == "SetupAddFunctions":
-            function_data_path = self._data_file_path      
-        
-        if entitytype_data_path:
+            self.setup_AddFunctions()
+        return status
+
+    def setup_EntityAction(self):
+        if self._data_file_path:
             """------Usage: 1. (X) Create Entity Type - using a json payload-----"""
-            operations_completed['create_entity'] = False
-            with open(entitytype_data_path, 'r') as f:
+            with open(self._data_file_path, 'r') as f:
                 try:
-                    op_create = entitytype.create_custom_entitytype(f.read(), 
-                                                                    credentials = self._credentials)
-                    print(f'ret_code is {op_create}. \nEntity created successfully')
-                    operations_completed['create_entity'] = True
+                    op_create = entitytype.create_custom_entitytype(f.read(),
+                                                                    credentials=self._credentials)
+                    self.logger.info(f'ret_code is {op_create}. \nEntity created successfully')
+                    return True
                 except Exception as msg:
-                    print(f'FAILED STEP: {msg}\nFailed create Entity Type operation')
+                    self.logger.debug(f'FAILED STEP: {msg}\nFailed create Entity Type operation')
+                    return False
 
-        if constants_data_path:
+    def setup_AddFunctions(self):
+        if self._data_file_path:
             """------Usage: 2. (X) Add Function - using a json payload------"""
-            operations_completed['add_functions'] = False
-            with open(function_data_path, 'r') as f:
+            with open(self._data_file_path, 'r') as f:
                 try:
-                    op_add = kpifunction.add_functions(f.read(), 
-                                                       credentials = self._credentials)
-                    print(f'ret_code is {op_add}. \nKPI Functions added successfully')
-                    operations_completed['add_functions'] = True
+                    op_add = kpifunction.add_functions(f.read(),
+                                                       credentials=self._credentials)
+                    self.logger.info(f'ret_code is {op_add}. \nKPI Functions added successfully')
+                    return True
                 except Exception as msg:
-                    print(f'FAILED STEP: {msg}\nFailed add functions operation')
-                    
-        if function_data_path:
-            """------Usage: 3. (X) Add Dimension - using a json payload------"""
-            operations_completed['add_dimension_data'] = False
-            with open(dimension_data_path, 'r') as f:
-                try:
-                    op_add = dimension.add_dimensions_data(f.read(), 
-                                                           credentials = self._credentials)
-                    print(f'ret_code is {op_add}. \nDimension data added successfully')
-                    operations_completed['add_dimension_data'] = True
-                except Exception as msg:
-                    print(f'FAILED STEP: {msg}\nFailed add dimension data operation')
-                    
-        if dimension_data_path:
-            """------Usage: 4. (X) Create Constants - using a json payload------"""
-            operations_completed['create_constants'] = False
-            with open(constants_data_path, 'r') as f:
-                try:
-                    op_create = constants.create_constants(f.read(), 
-                                                           credentials = self._credentials)
-                    print(f'ret_code is {op_create}. \nConstants created successfully')
-                    operations_completed['create_constants'] = True
-                except Exception as msg:
-                    print(f'FAILED STEP: {msg}\nFailed Create Constants operation')
+                    self.logger.debug(f'FAILED STEP: {msg}\nFailed add functions operation')
+                    return False
 
-        """----------STATUS----------"""
-        print('RESULT :')
-        for name, status in operations_completed.items():
-            print(f'Operation: {name} status: {status}')
-            if status == True:
-                success = True
-            else :
-                success = False
-                
-        return success
+    def setup_AddDimensions(self):
+        if self._data_file_path:
+            """------Usage: 3. (X) Add Dimension - using a json payload------"""
+            with open(self._data_file_path, 'r') as f:
+                try:
+                    op_add = dimension.add_dimensions_data(f.read(),
+                                                           credentials=self._credentials)
+                    self.logger.info(f'ret_code is {op_add}. \nDimension data added successfully')
+                    return True
+                except Exception as msg:
+                    self.logger.debug(f'FAILED STEP: {msg}\nFailed add dimension data operation')
+                    return False
+
+    def setup_AddConstants(self):
+        if self._data_file_path:
+            """------Usage: 4. (X) Create Constants - using a json payload------"""
+            with open(self._data_file_path, 'r') as f:
+                try:
+                    op_create = constants.create_constants(f.read(),
+                                                           credentials=self._credentials)
+                    self.logger.info(f'ret_code is {op_create}. \nConstants created successfully')
+                    return True
+                except Exception as msg:
+                    self.logger.debug(f'FAILED STEP: {msg}\nFailed Create Constants operation')
+                    return False
